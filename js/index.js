612f23404f8ff4f007793836098dcdf81969c931
biggie96 <biggieemmanuel@gmail.com>
2015-10-12 16:14:57 -0400

    changed home page animation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 
 $(document).ready(function(){
-    $( "#first_name, #last_name" ).hide(); 
+    /*$( "#first_name, #last_name" ).hide(); 
     $( "#first_name, #last_name" ).fadeIn(3000, function(){
         document.getElementById("real_name").style.color = "#333"; 
-    });
+    });*/
+    $("#real_name").hide().fadeIn(2500);
 });
 
